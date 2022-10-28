423d4fe8838fc7e62969b9108b208838ee538668
bbawuah <brianbawuah@icloud.com>
2022-10-28 21:45:42 +0200

    Initial Commit

---
+++ Computer-Graphics-101/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Computer-Graphics-101
+//
+//  Created by Brian Bawuah on 28/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
